c3b71fafac1c236353ab980c46aa6f14300af990
Alexander Ilyin <alexander.ilyin11@gmail.com>
2020-08-11 17:37:58 +0300

    Update resistor-color-duo.rb

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -2,9 +2,6 @@ class ResistorColorDuo
   def self.value(bands)
     colors = %w[black brown red orange yellow green blue violet grey white]
 
-    value1 = colors.index(bands[0])
-    value2 = colors.index(bands[1])
-
-    number = "#{value1}#{value2}".to_i
+    "#{colors.index(bands[0])}#{colors.index(bands[1])}".to_i
   end
 end
