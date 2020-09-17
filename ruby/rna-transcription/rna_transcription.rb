8eef4a0a934d95bd9384442ab55218b0b9557a5e
Alexander Ilyin <alexander.ilyin11@gmail.com>
2020-09-17 07:05:20 +0300

    Update rna_transcription.rb

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,19 +1,7 @@
-class Complement
-  def self.of_dna(dna_strand)
-    new(dna_strand).of_dna
-  end
+module Complement
+  module_function
 
-  private
-
-  attr_reader :dna_strand
-
-  def initialize(dna_strand)
-    @dna_strand = dna_strand
-  end
-
-  public
-
-  def of_dna
+  def of_dna(dna_strand)
     dna_strand.gsub(/[GCTA]/, 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U')
   end
 end
